43c0a777e6d6be43a659efd66ce82890ec6cd936
DIPANJAN DAS <31871028+dasdipanjan@users.noreply.github.com>
2020-08-11 03:07:46 +0530

    README and Code Doc
    Description: README file modified and code documentation has been added.

--- README.md
+++ README.md
@@ -8,4 +8,13 @@ For this program and rest other programs I have used *Oracle Java SDK-11*, Java 
 
 ### 2. Improved Hello World Program 
 This program is an advanced version of hello-world program. In this program I am dynamically adding Label, TextField, Label, Button to VBox. And Later on VBox is added to Scene and Scene is added to Stage.
-Few events have been attached to Buttons and they are functional.
+Few events have been attached to Buttons and they are functional. check the [improved-hello-world](https://github.com/dasdipanjan/java-fx-repo/tree/master/improved-hello-world) source code for all information.
+
+### 3. Java FX Application Life Cycle Program
+There are three important methods are in Java Fx's Application class. Important methods are following.
+    1. init()
+    2. start(Stage primaryStage)
+    3. stop()
+
+Java FX runtime creates so many threads in JavaFX application's life cycle. But among somany JavaFX launcher and application threads are very important. JavaFX-Launcher thread calls init() method and application thread calls start and stop method. In init() method you can initialize resources but you are not allowed to create Scene or Stage in this method. JavaFX Application Thread calls start(Stage primaryStage) and stop() method. But default constructor is called by JavaFx Application Thread.
+For more details please check the [life-cycle](https://github.com/dasdipanjan/java-fx-repo/tree/master/fx-lifecycle) code for more information.
--- fx-lifecycle/src/main/java/com/dd/fx/lifecycle/FxLifeCycle.java
+++ fx-lifecycle/src/main/java/com/dd/fx/lifecycle/FxLifeCycle.java
@@ -9,19 +9,40 @@ import javafx.stage.Stage;
 
 public class FxLifeCycle extends Application {
 
+    /**
+     * This constructor is the first entry point of the execution.
+     * Caller thread name: JavaFX Application Thread
+     */
     public FxLifeCycle() {
         System.out.println("Default constructor of FxLifeCycle class.");
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Execution Thread Name : "+threadName);
     }
 
+    /**
+     * This method is called after default constructor.
+     * Caller thread name:  JavaFX-Launcher
+     * @throws Exception
+     */
     @Override
     public void init() throws Exception {
         super.init();
         System.out.println("Init method of the application class has been executed.");
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Execution Thread Name : "+threadName);
     }
 
+    /**
+     * This method is called after init() method.
+     * Caller thread name: JavaFX Application Thread
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         System.out.println("start(Stage primaryStage) method of the application class has been executed.");
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Execution Thread Name : "+threadName);
         Button exitButton = new Button("EXIT APPLICATION");
         exitButton.setOnAction(e -> {
             Platform.exit();
@@ -34,9 +55,16 @@ public class FxLifeCycle extends Application {
         primaryStage.show();
     }
 
+    /**
+     * This method is called after start(Stage primaryStage) method.
+     * Caller thread name: JavaFX Application Thread
+     * @throws Exception
+     */
     @Override
     public void stop() throws Exception {
         super.stop();
         System.out.println("stop() method of the application class has been executed.");
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Execution Thread Name : "+threadName);
     }
 }
